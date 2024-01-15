ba8f9ac8df898f10a5ad3d2efd4ef8f806c1d942
AlexP0112 <alex.podaruu@gmail.com>
2024-01-15 15:37:56 +0200

    added cpu load metric explanation in home screen

--- app/src/main/java/com/example/powermanager/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/powermanager/ui/screens/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.example.powermanager.ui.screens
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,13 +9,23 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Card
 import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -23,6 +34,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import com.example.powermanager.R
@@ -53,6 +65,8 @@ fun HomeScreen(
             .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        val isCPULoadInfoDialogOpen = remember { mutableStateOf(false) }
+
         // ==============  battery and uptime section  ===================== //
         SectionHeader(
             sectionName = stringResource(R.string.battery_and_uptime)
@@ -131,6 +145,8 @@ fun HomeScreen(
             rightText = model.getNumCores().toString()
         )
 
+        Spacer(modifier = Modifier.height(10.dp))
+
         // cores frequencies
         homeScreenInfo.value.cpuFrequenciesGHz.forEachIndexed { index, frequency ->
             SectionMember(
@@ -139,17 +155,51 @@ fun HomeScreen(
             )
         }
 
-        // cpu load
-        SectionMember(
-            leftText = stringResource(R.string.cpu_load),
-            rightText = homeScreenInfo.value.cpuLoad.toString()
-        )
+        // cpu load, with info button that explains its meaning
+        Row (
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Row (
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                Text(
+                    text = stringResource(R.string.cpu_load),
+                    textAlign = TextAlign.Center,
+                    fontSize = 18.sp
+                )
+                IconButton(onClick = {
+                    isCPULoadInfoDialogOpen.value = true
+                }) {
+                    Icon(
+                        imageVector = Icons.Default.Info,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(20.dp)
+                            .align(Alignment.CenterVertically)
+                    )
+                }
+            }
+            Text(
+                text = homeScreenInfo.value.cpuLoad.toString(),
+                textAlign = TextAlign.Center,
+                fontSize = 18.sp
+            )
+        }
 
+        // button that takes you to statistics screen
         GoToChartButton(
             onClick = {
                 navController.navigate(STATISTICS_SCREEN_NAME)
             }
         )
+
+        if (isCPULoadInfoDialogOpen.value) {
+            InfoDialogForCpuLoad {
+                isCPULoadInfoDialogOpen.value = false
+            }
+        }
     }
 }
 
@@ -220,5 +270,34 @@ fun GoToChartButton(
             text = stringResource(R.string.go_to_charts),
             textAlign = TextAlign.Center
         )
+    }
+}
+
+@Composable
+fun InfoDialogForCpuLoad(
+    onDismissRequest: () -> Unit
+) {
+    Dialog(
+        onDismissRequest = onDismissRequest
+    ) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(220.dp)
+                .padding(16.dp),
+            shape = RoundedCornerShape(16.dp),
+            border = BorderStroke(
+                width = 2.dp,
+                color = MaterialTheme.colorScheme.onPrimaryContainer
+            )
+        ) {
+            Text(
+                text = stringResource(R.string.cpu_load_explanation),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(5.dp)
+                    .wrapContentSize(Alignment.Center),
+            )
+        }
     }
 }
