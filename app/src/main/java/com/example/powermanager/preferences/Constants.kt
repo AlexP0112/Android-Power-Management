d6f35e9fa1d223ecbaf99dab735786cb038098f7
AlexP0112 <alex.podaruu@gmail.com>
2024-01-16 20:58:53 +0200

    wip on preferences

--- app/src/main/java/com/example/powermanager/preferences/Constants.kt
+++ app/src/main/java/com/example/powermanager/preferences/Constants.kt
@@ -5,3 +5,9 @@ enum class LoadAverageTypes {
     LAST_FIVE_MINUTES,
     LAST_FIFTEEN_MINUTES
 }
+
+const val HOME_SCREEN_SAMPLING_PERIOD_ID = "home_screen_sampling_period"
+val HOME_SCREEN_SAMPLING_PERIOD_ALLOWED_VALUES = listOf(500L, 1000L, 2000L, 4000L, 10000L)
+const val HOME_SCREEN_SAMPLING_PERIOD_DEFAULT_VALUE = 2000L
+
+const val LOAD_AVERAGE_TYPE_ID = "load_average_type"
--- app/src/main/java/com/example/powermanager/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/example/powermanager/ui/screens/SettingsScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.powermanager.R
+import com.example.powermanager.preferences.allPreferences
 
 @Composable
 fun SettingsScreen(
@@ -35,5 +36,11 @@ fun SettingsScreen(
             fontWeight = FontWeight.Bold,
             fontSize = 20.sp
         )
+        
+        // the actual preferences
+        allPreferences.map { preference ->
+            Text(text = stringResource(id = preference.nameStringId))
+            Text(text = stringResource(id = preference.descriptionStringId))
+        }
     }
 }
