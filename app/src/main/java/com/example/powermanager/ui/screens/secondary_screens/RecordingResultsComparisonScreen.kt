505f92980b607d9c8d2b85bba83a03f5c38203ab
AlexP0112 <alex.podaruu@gmail.com>
2024-03-13 20:28:07 +0200

    finished work on double line charts for recording results comparison screen

--- app/src/main/java/com/example/powermanager/ui/screens/secondary_screens/RecordingResultsComparisonScreen.kt
+++ app/src/main/java/com/example/powermanager/ui/screens/secondary_screens/RecordingResultsComparisonScreen.kt
@@ -1,18 +1,26 @@
 package com.example.powermanager.ui.screens.secondary_screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -23,6 +31,7 @@ import com.example.powermanager.recording.storage.RecordingResult
 import com.example.powermanager.ui.charts.static_charts.DoubleLineStaticChart
 import com.example.powermanager.ui.charts.utils.CustomAxisValuesOverrider
 import com.example.powermanager.ui.model.PowerManagerAppModel
+import com.example.powermanager.utils.getListMaximum
 import com.example.powermanager.utils.getListMinimum
 import kotlin.math.max
 import kotlin.math.min
@@ -44,14 +53,27 @@ fun RecordingResultsComparisonScreen(
     ) {
         val uiState by model.recordingScreensUiState.collectAsState()
 
+        // get the two results that are compared
         val leftResult: RecordingResult = model.getCurrentlySelectedRecordingResult()
         val rightResult: RecordingResult = model.getRecordingResultToCompareWith()
 
-        // determine charts boundaries
-        val minMemoryUsage = min(getListMinimum(leftResult.memoryUsedValues), getListMinimum(rightResult.memoryUsedValues))
-        val maxMemoryUsage = max(leftResult.peakMemoryUsed, rightResult.peakMemoryUsed)
+        // convert integer lists to float lists, as required for charts
+        val leftBatteryValuesFloat = leftResult.batteryChargeValues.map {it.toFloat()}
+        val rightBatteryValuesFloat = rightResult.batteryChargeValues.map { it.toFloat() }
+        val leftThreadCountValuesFloat = leftResult.numberOfThreadsValues.map { it.toFloat() }
+        val rightThreadCountValuesFloat = rightResult.numberOfThreadsValues.map { it.toFloat() }
 
-        // title of the screen
+        // determine charts boundaries
+        val minMemoryUsage = min(getListMinimum(leftResult.memoryUsedValues), getListMinimum(rightResult.memoryUsedValues)) - 0.02f
+        val maxMemoryUsage = max(leftResult.peakMemoryUsed, rightResult.peakMemoryUsed) + 0.02f
+        val minCpuLoad = min(getListMinimum(leftResult.cpuLoadValues), getListMinimum(rightResult.cpuLoadValues)) - 0.1f
+        val maxCpuLoad = max(leftResult.peakCpuLoad, rightResult.peakCpuLoad) + 0.1f
+        val minBatteryLevel = max(min(getListMinimum(leftBatteryValuesFloat), getListMinimum(rightBatteryValuesFloat)) - 100f, 0f)
+        val maxBatteryLevel = max(getListMaximum(leftBatteryValuesFloat), getListMaximum(rightBatteryValuesFloat)) + 100f
+        val minThreadCount = min(getListMinimum(leftThreadCountValuesFloat), getListMinimum(rightThreadCountValuesFloat)) - 100f
+        val maxThreadCount = max(getListMaximum(leftThreadCountValuesFloat), getListMaximum(rightThreadCountValuesFloat)) + 100f
+
+        // ====================== screen title ====================== //
         Text(
             modifier = Modifier.align(Alignment.CenterHorizontally),
             text = "${uiState.currentlySelectedRecordingResult} vs ${uiState.selectedToCompareRecordingResult}",
@@ -63,6 +85,38 @@ fun RecordingResultsComparisonScreen(
 
         // ====================== charts ====================== //
 
+        Divider(
+            modifier = Modifier
+                .fillMaxSize(),
+            thickness = 0.75.dp,
+            color = MaterialTheme.colorScheme.secondary
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        // legend
+        ChartsLegend(
+            firstRecordingName = uiState.currentlySelectedRecordingResult,
+            secondRecordingName = uiState.selectedToCompareRecordingResult
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        // battery level chart
+        Text(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            text = stringResource(R.string.battery_level_mah)
+        )
+
+        DoubleLineStaticChart(
+            firstInput = leftBatteryValuesFloat,
+            secondInput = rightBatteryValuesFloat,
+            customAxisValuesOverrider = CustomAxisValuesOverrider(
+                minYValue = minBatteryLevel,
+                maxYValue = maxBatteryLevel
+            )
+        )
+
         // memory chart
         Text(
             modifier = Modifier.align(Alignment.CenterHorizontally),
@@ -73,9 +127,97 @@ fun RecordingResultsComparisonScreen(
             firstInput = leftResult.memoryUsedValues,
             secondInput = rightResult.memoryUsedValues,
             customAxisValuesOverrider = CustomAxisValuesOverrider(
-                minYValue = minMemoryUsage - 0.02f,
-                maxYValue = maxMemoryUsage + 0.02f
+                minYValue = minMemoryUsage,
+                maxYValue = maxMemoryUsage
             )
         )
+
+        // CPU load chart
+        Text(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            text = stringResource(R.string.cpu_load)
+        )
+
+        DoubleLineStaticChart(
+            firstInput = leftResult.cpuLoadValues,
+            secondInput = rightResult.cpuLoadValues,
+            customAxisValuesOverrider = CustomAxisValuesOverrider(
+                minYValue = minCpuLoad,
+                maxYValue = maxCpuLoad
+            )
+        )
+
+        // number of threads chart, if the stat was included
+        if (leftThreadCountValuesFloat.isNotEmpty() && rightThreadCountValuesFloat.isNotEmpty()) {
+            Text(
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                text = stringResource(R.string.total_number_of_threads)
+            )
+
+            DoubleLineStaticChart(
+                firstInput = leftThreadCountValuesFloat,
+                secondInput = rightThreadCountValuesFloat,
+                customAxisValuesOverrider = CustomAxisValuesOverrider(
+                    minYValue = minThreadCount,
+                    maxYValue = maxThreadCount
+                )
+            )
+        }
+    }
+}
+
+@Composable
+fun ChartsLegend(
+    firstRecordingName: String,
+    secondRecordingName: String
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 16.dp)
+    ) {
+        Text(
+            text = stringResource(R.string.charts_legend)
+        )
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Spacer(modifier = Modifier.width(12.dp))
+
+            Icon(
+                painter = painterResource(id = R.drawable.horizontal_line),
+                contentDescription = null,
+                modifier = Modifier.scale(2f, 1f),
+                tint = MaterialTheme.colorScheme.secondary
+            )
+
+            Spacer(modifier = Modifier.width(20.dp))
+
+            Text(
+                text = firstRecordingName
+            )
+        }
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Spacer(modifier = Modifier.width(12.dp))
+
+            Icon(
+                painter = painterResource(id = R.drawable.horizontal_line),
+                modifier = Modifier.scale(2f, 1f),
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.tertiary
+            )
+
+            Spacer(modifier = Modifier.width(20.dp))
+
+            Text(
+                text = secondRecordingName
+            )
+        }
     }
 }
